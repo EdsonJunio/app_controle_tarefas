dd38780ef0ede50fc4c481bb1c6fad4e9337df20
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2023-11-10 15:28:23 -0300

    feat:  Verificando se o usuário está logado dentro dos métodos

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Tarefa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TarefaController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +21,27 @@ class TarefaController extends Controller
      */
     public function index()
     {
-        //
+       /* if (auth()->check()) { // verifica se o usuário esta autenticado
+            $id = auth()->user()->id;
+            $nome = auth()->user()->name;
+            $email = auth()->user()->email;
+
+            return "ID: $id | Nome: $nome | Email: $email";
+        } else {
+            return 'Voce nao esta logado no sistema';
+        }
+       */
+
+        if (Auth::check()) { // verifica se o usuário esta autenticado
+            $id = Auth::user()->id;
+            $nome = Auth::user()->name;
+            $email = Auth::user()->email;
+
+            return "ID: $id | Nome: $nome | Email: $email";
+        } else {
+            return 'Voce nao esta logado no sistema';
+        }
+        return 'Chegamos aqui';
     }
 
     /**

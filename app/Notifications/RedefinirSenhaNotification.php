deb6f479fc524bee6c3c27d7a16112880db4087b
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2023-11-13 09:22:35 -0300

    feat:  Configurando o envio de e-mails (Reset Password) - Parte 2

---
+++ app/Notifications/RedefinirSenhaNotification.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
+
+class RedefinirSenhaNotification extends Notification
+{
+    use Queueable;
+    public $token;
+    public $email;
+    public $name;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token, $email, $name)
+    {
+        $this->token = $token;
+        $this->email = $email;
+        $this->name = $name;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = 'http://localhost:8000/password/reset/'.$this->token.'?email='.$this->email;
+        $minutos = config('auth.passwords.'.config('auth.passwords.'.config('auth.defaults.passwords').'.expire'));
+        $saudacao = 'Olá ';
+        return (new MailMessage)
+            ->subject('Atualização de senha')
+            ->greeting($saudacao.$this->name)
+            ->line('Esqueceu a senha? Sem problemas, vamos resolver isso!!!')
+            ->action('Click aqui para modificar a senha', $url)
+            ->line('O Link a cima expira em '.$minutos.'minutos')
+            ->line('Caso você não tenha requisitado a alterações de senha, então nenhuma ação é necessária.')
+            ->salutation('até breve !');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}

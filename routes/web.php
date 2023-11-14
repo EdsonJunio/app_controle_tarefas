6e4b8433d335eed9b722ad44aefd0c56eed8187a
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2023-11-14 12:29:07 -0300

    feat: Verificação de e-mail (MustVerifyEmail)

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('tarefa', 'App\Http\Controllers\TarefaController');//->middleware('auth');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home')
+    ->middleware('verified');
+Route::resource('tarefa', 'App\Http\Controllers\TarefaController')
+->middleware('verified');//->middleware('auth');
 
 Route::get('/mensagem-teste', function () {
     return new MensagemTesteMail();

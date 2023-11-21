b6f1bf9f11287696582f4c9d012382ef27a674f5
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2023-11-21 13:54:11 -0300

    feat: Modificando a rota home da aplicação (redirectTo)

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Auth::routes(['verify' => true]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
     ->name('home')
     ->middleware('verified');
+
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController')
 ->middleware('verified');//->middleware('auth');
 
